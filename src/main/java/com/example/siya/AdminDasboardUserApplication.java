e465ff4e94494d5a0419daac48d853863cf9ffc9
siya uikey <siyauikey808@gmail.com>
2026-02-07 15:30:13 +0530

    profile

---
+++ src/main/java/com/example/siya/AdminDasboardUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.siya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminDasboardUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminDasboardUserApplication.class, args);
+	}
+
+}
